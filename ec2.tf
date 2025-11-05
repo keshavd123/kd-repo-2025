68b0da118ae24794b9872261844f22b5fc3260d5
keshavd123 <kddhengare@gmail.com>
2025-11-05 05:01:10 +0000

    adding code to dryrun branch

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "example" {
     ami = "ami-0a25a306450a2cba3"
     instance_type = "t2.nano"
     key_name = "roche-key"
-    tags_all = {
+    tags = {
         "Name" = "kd-vm-1" 
     }
   
@@ -12,5 +12,6 @@ resource "aws_instance" "example" {
 resource "local_file" "ec2_ip_file" {
   content  = aws_instance.example.public_ip
   filename = "${path.module}/ec2_public_ip.txt"
-
+  # making manual dependancy
+  depends_on = [ aws_instance.example ]
 }
