a49b609b432e43eb76a6553a2af98344217bf0b5
keshavd123 <kddhengare@gmail.com>
2025-11-05 11:30:26 +0000

    adding ci file

--- ec2.tf
+++ ec2.tf
@@ -24,19 +24,3 @@ resource "aws_instance" "example" {
   
 }
 
-
-
-resource "local_file" "ec2_ip_file" {
-  content  = aws_instance.example.public_ip
-  filename = "${path.module}/ec2_public_ip.txt"
-  # making manual dependancy
-  depends_on = [ aws_key_pair.ec2_key_pair ]
-}
-
-resource "local_file" "ec2_ip_rsa" {
-  content  = tls_private_key.rsa_key_4096.private_key_openssh
-  filename = "${path.module}/kd-key.pem"
-  file_permission = var.file-perm
-  # making manual dependancy
-  depends_on = [ aws_instance.example ]
-}
---
+++ file.tf
@@ -0,0 +1,14 @@
+resource "local_file" "ec2_ip_file" {
+  content  = aws_instance.example.public_ip
+  filename = "${path.module}/ec2_public_ip.txt"
+  # making manual dependancy
+  depends_on = [ aws_key_pair.ec2_key_pair ]
+}
+
+resource "local_file" "ec2_ip_rsa" {
+  content  = tls_private_key.rsa_key_4096.private_key_openssh
+  filename = "${path.module}/kd-key.pem"
+  file_permission = var.file-perm
+  # making manual dependancy
+  depends_on = [ aws_instance.example ]
+}
