5593bc9cfbc1171d052fd82ff77e688652eb44b1
keshavd123 <kddhengare@gmail.com>
2025-11-05 06:09:07 +0000

    moving state file to s3

--- providers.tf
+++ providers.tf
@@ -1,4 +1,12 @@
 terraform {
+
+  backend "s3" {
+    bucket = "keshav-terraform-state-file"
+    key ="dev/keshav/terraform.tfstate"
+    dynamodb_table = "keshav-roche-table"
+    region = "ap-southeast-2"
+    encrypt = true
+  }
   required_providers {
     aws = {
       source = "hashicorp/aws"
@@ -11,3 +19,4 @@ provider "aws" {
     # Configuration options
     region = "ap-southeast-2"
 }
+
