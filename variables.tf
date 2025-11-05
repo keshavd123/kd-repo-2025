040f33c550a52cd1be068cc89393caa493a735f0
keshavd123 <kddhengare@gmail.com>
2025-11-05 07:26:10 +0000

    variable implementation

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "kd-ami-id" {
+    type = string
+    description = "this is ami id"
+
+}
+
+variable "vm-size" {
+    type = string
+}
+
+variable "vm-name" {
+    type = string
+}
+
+variable "ec2-key-name" {
+    type = string
+  
+}
